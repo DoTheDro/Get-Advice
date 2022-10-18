cd07bdc5e55f7a76cf4b60d8a1b358ad513b33f4
DoTheDro <88122021+DoTheDro@users.noreply.github.com>
2022-10-18 23:28:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const container = document.querySelector('.custom-con');
-const anotherAdvice = document.querySelector('button');
+const anotherAdvice = document.querySelector('.advice-btn');
+const modeBtn = document.querySelector('#viewmode-btn');
 const h1 = document.createElement('h1');
 const randomAPI = "https://api.adviceslip.com/advice";
 
 anotherAdvice.addEventListener('click', (e) => {
     getRandomAdvice();
-})
+});
+
+modeBtn.addEventListener('click', (e) => {
+    if (modeBtn.getAttribute('class') == "light-mode") {
+        modeBtn.setAttribute('class', 'dark-mode');
+        modeBtn.textContent = "Light Mode";
+        document.body.style.backgroundColor = "#000";
+        document.body.style.transition = "0.5s";
+        h1.style.color = "#FFF";
+        anotherAdvice.style.backgroundColor = "#00FFFF";
+    } else {
+        modeBtn.setAttribute('class', 'light-mode');
+        modeBtn.textContent = "Dark Mode";
+        document.body.style.backgroundColor = "#FFF";
+        h1.style.color = "#000";
+        anotherAdvice.style.backgroundColor = "#93FFFF";
+    }
+});
 
 const getRandomAdvice = () => {
     fetch(randomAPI)
@@ -15,8 +33,6 @@ const getRandomAdvice = () => {
         h1.textContent = data.slip.advice;
         container.append(h1);
     })
-}
+};
 
 getRandomAdvice();
-
-//#00FFFF
